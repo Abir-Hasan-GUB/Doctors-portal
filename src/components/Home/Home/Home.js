21d1df961666b9f439c7467b00ee8936b6c623d1
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-09 14:40:11 +0600

    Initial Git and done half of home page

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Hader/Header';
+import Sarvices from '../Sarvices/Sarvices';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Sarvices></Sarvices>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Home/Sarvices/Sarvices.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import flouride from '../../../images/flouride.png';
+import cavity from '../../../images/cavity.png';
+import whiteanin from '../../../images/whiteanin.png';
+import ServicesDetails from '../ServicesDetails/ServicesDetails';
+
+const serviceData = [
+    {
+        name: 'Fluoride Treatment',
+        img: flouride,
+    },
+    {
+        name: 'Cavity Filling',
+        img: cavity,
+    },
+    {
+        name: 'Teeth whitening',
+        img: whiteanin,
+    },
+]
+
+const Sarvices = () => {
+    return (
+        <section className ="services-container mt-5 container">
+          <div className="text-center">
+          <h5 style={{color: '#1CC7C1'}}>OUR SERVICES</h5>
+            <h2>Services we provide</h2>
+          </div>
+          <div className = "d-flex justify-content-center">
+           <div className="w-75 row mt-5 pt-3">
+           {
+               serviceData.map(sarvice => <ServicesDetails sarvice = {sarvice}></ServicesDetails>)
+           }
+           </div>
+       </div>
+        </section>
+    );
+};
+
+export default Sarvices;
