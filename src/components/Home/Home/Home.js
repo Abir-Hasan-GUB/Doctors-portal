e29915ff69c80b5e9cac1bc3f0890aba74a8b118
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-11 00:28:02 +0600

    Chenge Location of contacts part & initialize footer part

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Contacts from '../../Contacts/Contacts';
-import Footer from '../../Contacts/Contacts';
+import Footer from '../../Footer/Footer';
 import Appointment from '../Appointment/Appointment';
 import Blog from '../Blog/Blog';
+import Contacts from '../Contacts/Contacts';
 import FeaturedService from '../FeaturedService/FeaturedService';
 import Header from '../Hader/Header';
 import OurDoctors from '../OurDoctors/OurDoctors';
@@ -20,7 +20,7 @@ const Home = () => {
             <Blog></Blog>
             <OurDoctors></OurDoctors>
             <Contacts></Contacts>
-
+            <Footer></Footer>
         </div>
     );
 };
