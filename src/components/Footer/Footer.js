9828866d47b9515c065ea97a65865af11d436a71
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-11 11:19:54 +0600

    Footer part complete with finishd all part of home page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
 
+const facebook = <FontAwesomeIcon icon={faFacebookF} />
+const google = <FontAwesomeIcon icon={faInstagram} />
+const twitter = <FontAwesomeIcon icon={faGooglePlusG} />
+
+{/* <i class="fab fa-facebook-f"></i>
+<i class="fab fa-google-plus-g"></i>
+<i class="fab fa-twitter"></i> */}
 const Footer = () => {
     return (
-        <div className = "container">
-            <h1>Footer</h1>
+        <div className = "container footerDiv">
+            <div className="row">
+                <div className="col-md-3 one">
+                <ul className = "footerList">
+                    <li>Emergency Dental Care</li>
+                    <li>Check Up</li>
+                    <li>Treatment of Personal Diseases</li>
+                    <li>Tooth Extraction</li>
+                    <li>Check Up</li>
+                </ul>
+                </div>
+                <div className="col-md-3 two ">
+                <h5 className="footerTitle">Services</h5>
+                <ul className = "footerList">
+                    <li>Emergency Dental Care</li>
+                    <li>Check Up</li>
+                    <li>Treatment of Personal Diseases</li>
+                    <li>Tooth Extraction</li>
+                    <li>Check Up</li>
+                    <li>Check Up</li>
+                    <li>Check Up</li>
+                </ul>
+                </div>
+                <div className="col-md-3 three">
+                <h5 className="footerTitle">Oral Health</h5>
+                <ul className = "footerList">
+                    <li>Emergency Dental Care</li>
+                    <li>Check Up</li>
+                    <li>Treatment of Personal Diseases</li>
+                    <li>Tooth Extraction</li>
+                    <li>Check Up</li>
+                    <li>Check Up</li>
+                    <li>Check Up</li>
+                </ul>
+                </div>
+                <div className="col-md-3 four">
+                <h5 className="footerTitle">Our Address</h5>
+                <ul className = "footerList">
+                    <li>New York - 101010 Hudson Yards</li>
+                </ul>
+                <div className="icons">
+                    <ul>
+                        <li>{facebook}</li>
+                        <li>{google}</li>
+                        <li>{twitter}</li>
+                    </ul>
+                </div>
+                <div className="callNow">
+                    <h6  className = "text-secondary">Call Now</h6>
+                    <p>+2025550295</p>
+                </div>
+                </div>
+                <div className="copyright">
+                <p> © Copyright {new Date().getFullYear()} All Rights Reserved by "Abir Hasan"</p>
+                </div>
+            </div>
         </div>
     );
 };
